c6e65c9cf001ce95f8be4bfacdce1823c0ea7a85
sfh1980 <96262139+sfh1980@users.noreply.github.com>
2022-12-12 10:04:45 -0500

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -35,7 +35,7 @@ HANGMAN_PICS = ['''
    /|\  |
    / \  |
        ===''']
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep weasel whale wolf wombat zebra' .split()
+words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep weasel whale wolf wombat zebra'.split()
 
 def getRandomWord(wordList):
     # This function returns a random string from the passed list of strings.
@@ -46,19 +46,19 @@ def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_PICS[len(missedLetters)])
     print()
 
-    print('Missed letters:', end='')
+    print('Missed letters:', end=' ')
     for letter in missedLetters:
-        print(letter, end='')
+        print(letter, end=' ')
     print()
 
-    blanks = '_' *len(secretWord)
+    blanks = '_' * len(secretWord)
 
     for i in range(len(secretWord)): # Replace blanks with correctly guess letters.
         if secretWord[i] in correctLetters:
             blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
 
     for letter in blanks: #show the secret word with spaced in between each letter
-        print(letter, end='')
+        print(letter, end=' ')
     print()
 
 def getGuess(alreadyGuessed):
@@ -67,7 +67,7 @@ def getGuess(alreadyGuessed):
         print('Guess a letter.')
         guess = input()
         guess = guess.lower()
-        if len(guess) !=1:
+        if len(guess) != 1:
             print('Please enter a single letter.')
         elif guess in alreadyGuessed:
             print('You have already guessed that letter. Choose again.')
@@ -103,9 +103,9 @@ while True:
             if secretWord[i] not in correctLetters:
                 foundAllLetters = False
                 break
-            if foundAllLetters:
-                print('Yes. The secret word is "' + secretWord + '"! You have won!')
-                gameIsDone = True
+        if foundAllLetters:
+            print('Yes. The secret word is "' + secretWord + '"! You have won!')
+            gameIsDone = True
     else:
         missedLetters = missedLetters + guess
 
